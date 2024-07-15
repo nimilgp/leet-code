921455ed53da4f259f3debf3b23be31a145ac112
nimil <nimilgeorgepradeepngp@gmail.com>
2024-07-15 21:25:34 +0530

    You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
    On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.

---
+++ top-interview-150/0122.BestTimeToBuyAndSellStock2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l = 0
+        r = 1
+        profit = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit += prices[r] - prices[l]
+            l = r
+            r += 1
+        return profit
