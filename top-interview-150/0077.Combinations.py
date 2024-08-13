6e47cc8356981125aab4b720964d352b4bbdaa4b
nimil <nimilgeorgepradeepngp@gmail.com>
2024-08-13 21:26:06 +0530

    iven two integers n and k, return all possible combinations of k numbers chosen from the range [1, n]

---
+++ top-interview-150/0077.Combinations.py
@@ -0,0 +1,13 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        ans = []
+        def backtrack(start, comb):
+            if len(comb) == k:
+                ans.append(comb[:])
+                return
+            for i in range(start, n+1):
+                comb.append(i)
+                backtrack(i+1, comb)
+                comb.pop()
+        backtrack(1,[])
+        return ans
